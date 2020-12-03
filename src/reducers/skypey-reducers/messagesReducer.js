4b97ecb61484e9679375d11d35028176fc3939b1
Oluwaferanmi Adedokun <adedokunoluwaferanmi@gmail.com>
2020-12-03 21:40:10 +0100

    feat: messages reducer action & typing state

--- src/reducers/skypey-reducers/messagesReducer.js
+++ src/reducers/skypey-reducers/messagesReducer.js
@@ -1,5 +1,25 @@
 import { getMessages } from '../../static-data'
+import { SEND_MESSAGE } from '../../actions/constants/action-types'
 
 export default function messages(state = getMessages(), action) {
-  return state
+  switch(action.type) {
+    case SEND_MESSAGE:
+      const { message, userId } = action.payload;
+      const allUserMessages = state[userId];      
+      const number = Number(Object.keys(allUserMessages).pop()) + 1      
+
+      return {
+        ...state,
+        [userId]: {
+          ...allUserMessages,
+          [number]: {
+            number,
+            text: message,
+            is_user_msg: true,
+          }
+        }
+      }
+    default:
+      return state
+  }
 }
