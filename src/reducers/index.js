5fac5be62bb9a34fc123a8e32cc49c21f331e2d7
Oluwaferanmi Adedokun <adedokunoluwaferanmi@gmail.com>
2020-12-03 14:44:10 +0100

    reducers for tying state value

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,10 +4,12 @@ import user from './skypey-reducers/user'
 import contacts from './skypey-reducers/contacts'
 import activeUserId from './skypey-reducers/activeUserId'
 import messages from './skypey-reducers/messagesReducer'
+import typing from './skypey-reducers/typingReducer'
 
 export default combineReducers({
   user,
   messages,
   contacts,
-  activeUserId
+  activeUserId,
+  typing
 });
